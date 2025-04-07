1e3bbdf38145f9e7b90b862db2c75f03b57e4e5b
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-04-07 10:16:08 +0530

    Created DTOs and Model class

---
+++ fitness-userservice/src/main/java/com/fitness/userservice/FitnessUserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessUserserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessUserserviceApplication.class, args);
+    }
+
+}
