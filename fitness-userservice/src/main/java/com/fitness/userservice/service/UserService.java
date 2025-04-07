2dc701e011993a3e6cf61fd18ae1a876224d8b22
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-04-07 10:22:52 +0530

    Created Register User Profile API

---
+++ fitness-userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -0,0 +1,47 @@
+package com.fitness.userservice.service;
+
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.model.User;
+import com.fitness.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserResponse getUserProfile(String userId) {
+
+
+
+    }
+
+    public UserResponse register(RegisterRequest request) {
+
+        if(userRepository.existsByEmail(request.getEmail())) {
+            throw  new RuntimeException("User email already present");
+        }
+
+        User user = new User();
+
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+
+        User savedUser = userRepository.save(user);
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(savedUser.getId());
+        userResponse.setPassword(savedUser.getPassword());
+        userResponse.setEmail(savedUser.getEmail());
+        userResponse.setFirstName(savedUser.getFirstName());
+        userResponse.setLastName(savedUser.getLastName());
+        userResponse.setCreatedAt(savedUser.getCreatedAt());
+        user.setUpdatedAt(savedUser.getUpdatedAt());
+
+        return userResponse;
+    }
+}
