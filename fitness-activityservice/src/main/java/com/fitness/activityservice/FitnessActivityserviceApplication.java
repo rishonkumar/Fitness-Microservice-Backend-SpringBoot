8d1ff90380ae5ec0617bea251d02c8d9a6d4fe84
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-04-07 17:18:48 +0530

    Setting up Activity Service Setup

---
+++ fitness-activityservice/src/main/java/com/fitness/activityservice/FitnessActivityserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.activityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessActivityserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessActivityserviceApplication.class, args);
+    }
+
+}
