886dcf0746f3f81442aa8f8402958e97b55edc94
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-04-07 10:40:14 +0530

    Get Profile API

--- fitness-userservice/src/main/java/com/fitness/userservice/dto/RegisterRequest.java
+++ fitness-userservice/src/main/java/com/fitness/userservice/dto/RegisterRequest.java
@@ -5,14 +5,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-@Data
-public class RegisterRequest {
-    @NotBlank
-    @Email
-    private String email;
-    @NotBlank
-    @Size(min = 6)
-    private String password;
-    private String firstName;
-    private String lastName;
-}
+    @Data
+    public class RegisterRequest {
+        @NotBlank
+        @Email
+        private String email;
+        @NotBlank
+        @Size(min = 6)
+        private String password;
+        private String firstName;
+        private String lastName;
+    }
--- fitness-userservice/src/main/java/com/fitness/userservice/service/UserService.java
+++ fitness-userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -15,7 +15,18 @@ public class UserService {
 
     public UserResponse getUserProfile(String userId) {
 
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
 
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(user.getId());
+        userResponse.setPassword(user.getPassword());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setCreatedAt(user.getCreatedAt());
+        user.setUpdatedAt(user.getUpdatedAt());
+
+        return userResponse;
 
     }
 
