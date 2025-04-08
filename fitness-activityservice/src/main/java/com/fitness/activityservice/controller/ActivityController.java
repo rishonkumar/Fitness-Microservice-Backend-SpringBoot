3284590ab7972a543da4fcbebe122cdd04f189b5
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-04-08 09:45:00 +0530

    Added feature getActivity and get User Activities

--- fitness-activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
+++ fitness-activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -6,10 +6,9 @@ import com.fitness.activityservice.dto.ActivityResponse;
 import com.fitness.activityservice.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/activities")
@@ -23,6 +22,16 @@ public class ActivityController {
 
         return ResponseEntity.ok(activityService.trackActivity(activityRequest));
 
-
     }
+
+    @GetMapping
+    public ResponseEntity<List<ActivityResponse>> getUserActivities(@RequestHeader("X-User-ID") String userId) {
+        return ResponseEntity.ok(activityService.getUserActivites(userId));
+    }
+
+    @GetMapping("/{activityId}")
+    public ResponseEntity<ActivityResponse> getActivity(@PathVariable String activityId) {
+        return ResponseEntity.ok(activityService.getActivityById(activityId));
+    }
+
 }
